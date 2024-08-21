49281ac55a37df661cb189088a6b6b21a0359238
dom7kim <dominic.kim@mathpresso.com>
2024-08-21 13:12:31 +0900

    speed insight

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -30,6 +31,7 @@ export default function RootLayout({ children }) {
         <div className="min-h-screen flex flex-col">
           {children}
         </div>
+        <SpeedInsights />
       </body>
     </html>
   );
